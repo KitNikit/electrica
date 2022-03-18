381b35bdd4d9ae6621fc18a365a9d21bceffd1de
Nikita <vclan@mail.ru>
2022-03-18 12:58:16 +0300

    mobile

--- pages/Map.vue
+++ pages/Map.vue
@@ -7,7 +7,7 @@
     <map>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.7313880156476!2d38.27866211497088!3d54.01029853462468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41369d4df2b78623%3A0x3dff32ee92a329ab!2z0K3Qu9C10LrRgtGA0LjQutCwIOKEljE!5e0!3m2!1sru!2sru!4v1646388820392!5m2!1sru!2sru"
-        width="80%"
+        width="90%"
         height="450"
         style="border: 0"
         allowfullscreen=""
@@ -23,11 +23,16 @@ export default {};
 
 <style>
 h1 {
-  padding-top: 30px;
+  padding-top: 70px;
   text-align: center;
 }
 iframe {
   display: block;
   margin: 50px auto;
+}
+@media (max-width: 768px) {
+  h1 {
+    font-size: 24px;
+  }
 }
 </style>
